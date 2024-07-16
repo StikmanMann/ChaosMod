277f27bac780c769f64a979ec5cf86bd674c52bf
LaptopmanMann <florianagario2@gmail.com>
2024-07-16 13:40:20 +0200

    updated type or die

--- scripts/ChaosMod/Events/typeOrDie.js
+++ scripts/ChaosMod/Events/typeOrDie.js
@@ -1,50 +1,74 @@
 import { world, system, } from "@minecraft/server";
 import { GlobalVars } from "globalVars";
-const typeOrDie = () => {
-    const statements = [
-        "Wii U",
-        "Minecraft",
-        "AliveJy is bad",
-        "erm what the sigma",
-        "Nintendo 3DS",
-        "Mario",
-        "Jay-Z",
-        "Pneumonoultramicroscopicsilicovolcanoconiosis",
-        "Fair Game",
-        "I love Chaos Minecraft!! :)",
-    ];
-    const idx = Math.floor(Math.random() * (9 - 0) + 0);
-    world.sendMessage("Say this or death :) : " + statements[idx]);
-    world.beforeEvents.chatSend.subscribe((eventData) => {
-        system.run(() => {
-            eventData.cancel = true;
-            const message = eventData.message;
-            const player = eventData.sender;
-            if (message == statements[idx]) {
-                player.addTag("isSafe");
-                player.sendMessage("ur safe now");
-            }
-        });
+let currentStatement;
+const statements = [
+    "Wii U",
+    "Minecraft",
+    "AliveJy is bad",
+    "erm what the sigma",
+    "Nintendo 3DS",
+    "Mario",
+    "Jay-Z",
+    "Pneumonoultramicroscopicsilicovolcanoconiosis",
+    "Fair Game",
+    "I love Chaos Minecraft!! :)",
+];
+const succesMessages = [
+    "Great job at typing!",
+    "Wow your cognitive ability is amazing!",
+    "Wow this is crazy how you typed that!",
+    "With those typing skills you should sign up at a championship!",
+    `I don't even know myself how to type ${currentStatement}!`,
+];
+const loseMessages = [
+    "doesn't know how to type",
+    "suffers from carpal tunnel syndrome",
+];
+const chooseRandomStatement = () => {
+    return statements[Math.floor(Math.random() * statements.length)];
+};
+const chooseRandomSuccessMessage = () => {
+    return succesMessages[Math.floor(Math.random() * succesMessages.length)];
+};
+const chooseRandomLoseMessage = () => {
+    return loseMessages[Math.floor(Math.random() * loseMessages.length)];
+};
+currentStatement = chooseRandomStatement();
+const typeorDieChatEvent = (eventData) => {
+    system.run(() => {
+        const message = eventData.message;
+        const player = eventData.sender;
+        if (message == currentStatement) {
+            player.addTag("isSafe");
+            player.sendMessage(chooseRandomSuccessMessage());
+        }
+        //world.sendMessage(`§a${player.nameTag}§r: ${message}`);
     });
+};
+const typeOrDieStart = () => {
+    world.sendMessage(`§aSay this or death :) : §r${currentStatement}`);
+    world.beforeEvents.chatSend.subscribe(typeorDieChatEvent);
+};
+const typeOrDieStop = () => {
+    for (const player of GlobalVars.players) {
+        if (!player.hasTag("isSafe")) {
+            player.kill();
+            world.sendMessage(`§4${player.nameTag}§r ${chooseRandomLoseMessage()}`);
+        }
+        if (player.hasTag("isSafe")) {
+            player.removeTag("isSafe");
+        }
+    }
+    world.beforeEvents.chatSend.unsubscribe(typeorDieChatEvent);
+    currentStatement = chooseRandomStatement();
 };
 const typeOrDieTrying = {
     chaosEventId: "typeOrDie",
     chaosEventDisplayName: "Type the message or die trying",
     chaosEventUniqueId: "-1",
     chaosEventTime: 200,
-    onChaosStart: typeOrDie,
-    onChaosStop: () => {
-        for (const player of GlobalVars.players) {
-            if (player.hasTag("isSafe") == false) {
-                player.kill();
-            }
-        }
-        for (const player of GlobalVars.players) {
-            if (player.hasTag("isSafe")) {
-                player.removeTag("isSafe");
-            }
-        }
-    },
+    onChaosStart: typeOrDieStart,
+    onChaosStop: typeOrDieStop,
     onChaosTick: () => { },
 };
 export default typeOrDieTrying;
--- src/ChaosMod/Events/typeOrDie.ts
+++ src/ChaosMod/Events/typeOrDie.ts
@@ -3,37 +3,86 @@ import {
   world,
   system,
   Player,
+  ChatSendBeforeEvent,
 } from "@minecraft/server";
 import { IChaosEvent } from "ChaosMod/IChaosEvent";
 import { Logger } from "staticScripts/Logger";
 import { GlobalVars } from "globalVars";
 
-const typeOrDie = () => {
-  const statements = [
-    "Wii U",
-    "Minecraft",
-    "AliveJy is bad",
-    "erm what the sigma",
-    "Nintendo 3DS",
-    "Mario",
-    "Jay-Z",
-    "Pneumonoultramicroscopicsilicovolcanoconiosis",
-    "Fair Game",
-    "I love Chaos Minecraft!! :)",
-  ];
-  const idx = Math.floor(Math.random() * (9 - 0) + 0);
-  world.sendMessage("Say this or death :) : " + statements[idx]);
-  world.beforeEvents.chatSend.subscribe((eventData) => {
-    system.run(() => {
-      eventData.cancel = true;
-      const message = eventData.message;
-      const player = eventData.sender;
-      if (message == statements[idx]) {
-        player.addTag("isSafe");
-        player.sendMessage("ur safe now")
-      }
-    });
+let currentStatement: string;
+
+const statements = [
+  "Wii U",
+  "Minecraft",
+  "AliveJy is bad",
+  "erm what the sigma",
+  "Nintendo 3DS",
+  "Mario",
+  "Jay-Z",
+  "Pneumonoultramicroscopicsilicovolcanoconiosis",
+  "Fair Game",
+  "I love Chaos Minecraft!! :)",
+];
+
+const succesMessages = [
+  "Great job at typing!",
+  "Wow your cognitive ability is amazing!",
+  "Wow this is crazy how you typed that!",
+  "With those typing skills you should sign up at a championship!",
+  `I don't even know myself how to type ${currentStatement}!`,
+];
+
+const loseMessages = [
+  "doesn't know how to type",
+  "suffers from carpal tunnel syndrome",
+  "played twister with his fingers and lost",
+];
+
+const chooseRandomStatement = (): string => {
+  return statements[Math.floor(Math.random() * statements.length)];
+};
+
+const chooseRandomSuccessMessage = (): string => {
+  return succesMessages[Math.floor(Math.random() * succesMessages.length)];
+};
+
+const chooseRandomLoseMessage = (): string => {
+  return loseMessages[Math.floor(Math.random() * loseMessages.length)];
+};
+
+currentStatement = chooseRandomStatement();
+
+const typeorDieChatEvent = (eventData: ChatSendBeforeEvent) => {
+  system.run(() => {
+    const message = eventData.message;
+    const player = eventData.sender;
+    if (message == currentStatement) {
+      player.addTag("isSafe");
+      player.sendMessage(chooseRandomSuccessMessage());
+    }
+    //world.sendMessage(`§a${player.nameTag}§r: ${message}`);
   });
+};
+
+const typeOrDieStart = () => {
+  world.sendMessage(`§aSay this or death :) : §r${currentStatement}`);
+  world.beforeEvents.chatSend.subscribe(typeorDieChatEvent);
+};
+
+const typeOrDieStop = () => {
+  for (const player of GlobalVars.players) {
+    if (!player.hasTag("isSafe")) {
+      player.kill();
+      world.sendMessage(`§4${player.nameTag}§r ${chooseRandomLoseMessage()}`);
+    }
+    if (player.hasTag("isSafe")) {
+      player.removeTag("isSafe");
+    }
+  }
+
+  world.beforeEvents.chatSend.unsubscribe(typeorDieChatEvent);
+
+  currentStatement = chooseRandomStatement();
 };
 
 const typeOrDieTrying: IChaosEvent = {
@@ -44,19 +93,8 @@ const typeOrDieTrying: IChaosEvent = {
 
   chaosEventTime: 200,
 
-  onChaosStart: typeOrDie,
-  onChaosStop: () => {
-    for (const player of GlobalVars.players) {
-      if (player.hasTag("isSafe") == false) {
-        player.kill();
-      }
-    }
-    for (const player of GlobalVars.players) {
-      if (player.hasTag("isSafe")) {
-        player.removeTag("isSafe");
-      }
-    }
-},
+  onChaosStart: typeOrDieStart,
+  onChaosStop: typeOrDieStop,
   onChaosTick: () => {},
 };
 
