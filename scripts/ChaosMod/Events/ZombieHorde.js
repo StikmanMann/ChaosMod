0c4760219200cc038084841f4137bad43144d99e
florian <florianagario2@gmail.com>
2024-07-24 17:48:06 +0200

    Zombie horde lasts 60 seconds periosdically spawning zombies

--- scripts/ChaosMod/Events/ZombieHorde.js
+++ scripts/ChaosMod/Events/ZombieHorde.js
@@ -1,32 +1,41 @@
+import { system, world } from "@minecraft/server";
 import { GlobalVars } from "globalVars";
 import { VectorFunctions } from "staticScripts/vectorFunctions";
 /**This number will be squared */
 const zombieAmountLength = 5;
 const zombieHordeStart = () => {
+    world.sendMessage(`§4The zombies are coming!`);
+};
+const zombieDelay = 5;
+const zombieSpread = 50;
+const minimumDistance = 5;
+const zombieHordeTick = () => {
+    if (system.currentTick % zombieDelay != 0)
+        return;
     for (const player of GlobalVars.players) {
-        const offsetSpawnLocation = VectorFunctions.addVector(player.location, {
-            x: 0,
-            y: 5,
-            z: 0,
-        });
-        let halfLength = Math.floor(zombieAmountLength / 2);
-        for (let xOffset = -halfLength; xOffset <= halfLength; xOffset++) {
-            for (let zOffset = -halfLength; zOffset <= halfLength; zOffset++) {
-                player.dimension.spawnEntity("zombie", VectorFunctions.addVector(offsetSpawnLocation, {
-                    x: xOffset,
-                    y: 0,
-                    z: zOffset,
-                }));
-            }
+        let xOffset = Math.random() * zombieSpread - zombieSpread / 2;
+        if (xOffset < minimumDistance && xOffset > -minimumDistance) {
+            xOffset = minimumDistance;
         }
+        let zOffset = Math.random() * zombieSpread - zombieSpread / 2;
+        if (zOffset < minimumDistance && zOffset > -minimumDistance) {
+            zOffset = minimumDistance;
+        }
+        const spawnLocation = VectorFunctions.addVector(player.location, {
+            x: xOffset,
+            y: 5,
+            z: zOffset,
+        });
+        const zombie = player.dimension.spawnEntity("minecraft:zombie", spawnLocation);
+        zombie.addEffect("fire_resistance", 2000, { showParticles: false });
     }
 };
 export const zombieHorde = {
     chaosEventId: "zombieHorde",
     chaosEventDisplayName: "The zombies are coming",
-    chaosEventTime: 0,
+    chaosEventTime: 1200,
     chaosEventUniqueId: "-1",
     onChaosStart: zombieHordeStart,
     onChaosStop: () => { },
-    onChaosTick: () => { },
+    onChaosTick: zombieHordeTick,
 };
--- src/ChaosMod/Events/ZombieHorde.ts
+++ src/ChaosMod/Events/ZombieHorde.ts
@@ -1,31 +1,44 @@
+import { system, world } from "@minecraft/server";
 import { IChaosEvent } from "ChaosMod/IChaosEvent";
 import { GlobalVars } from "globalVars";
 import { VectorFunctions } from "staticScripts/vectorFunctions";
+import { workerData } from "worker_threads";
 
 /**This number will be squared */
 const zombieAmountLength: number = 5;
 
 const zombieHordeStart = () => {
-  for (const player of GlobalVars.players) {
-    const offsetSpawnLocation = VectorFunctions.addVector(player.location, {
-      x: 0,
-      y: 5,
-      z: 0,
-    });
+  world.sendMessage(`§4The zombies are coming!`);
+};
 
-    let halfLength = Math.floor(zombieAmountLength / 2);
-    for (let xOffset = -halfLength; xOffset <= halfLength; xOffset++) {
-      for (let zOffset = -halfLength; zOffset <= halfLength; zOffset++) {
-        player.dimension.spawnEntity(
-          "zombie",
-          VectorFunctions.addVector(offsetSpawnLocation, {
-            x: xOffset,
-            y: 0,
-            z: zOffset,
-          })
-        );
-      }
+const zombieDelay = 5;
+const zombieSpread = 50;
+const minimumDistance = 5;
+
+const zombieHordeTick = () => {
+  if (system.currentTick % zombieDelay != 0) return;
+
+  for (const player of GlobalVars.players) {
+    let xOffset = Math.random() * zombieSpread - zombieSpread / 2;
+    if (xOffset < minimumDistance && xOffset > -minimumDistance) {
+      xOffset = minimumDistance;
     }
+
+    let zOffset = Math.random() * zombieSpread - zombieSpread / 2;
+
+    if (zOffset < minimumDistance && zOffset > -minimumDistance) {
+      zOffset = minimumDistance;
+    }
+    const spawnLocation = VectorFunctions.addVector(player.location, {
+      x: xOffset,
+      y: 5,
+      z: zOffset,
+    });
+    const zombie = player.dimension.spawnEntity(
+      "minecraft:zombie",
+      spawnLocation
+    );
+    zombie.addEffect("fire_resistance", 2000, { showParticles: false });
   }
 };
 
@@ -33,11 +46,11 @@ export const zombieHorde: IChaosEvent = {
   chaosEventId: "zombieHorde",
   chaosEventDisplayName: "The zombies are coming",
 
-  chaosEventTime: 0,
+  chaosEventTime: 1200,
 
   chaosEventUniqueId: "-1",
 
   onChaosStart: zombieHordeStart,
   onChaosStop: () => {},
-  onChaosTick: () => {},
+  onChaosTick: zombieHordeTick,
 };
