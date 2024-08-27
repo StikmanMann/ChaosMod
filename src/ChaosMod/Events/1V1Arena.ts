9c77d3aa7043525d85c15b90de68a5a0bc23b5ef
LaptopmanMann <florianagario2@gmail.com>
2024-08-27 12:31:01 +0200

    s

--- src/ChaosMod/Events/1V1Arena.ts
+++ src/ChaosMod/Events/1V1Arena.ts
@@ -2,9 +2,28 @@ import { Vector3, world } from "@minecraft/server";
 import { IChaosEvent } from "ChaosMod/IChaosEvent";
 import { GlobalVars } from "globalVars";
 
+function shuffleArray<T>(array: Array<T>): Array<T> {
+  let currentIndex = array.length;
+
+  while (currentIndex !== 0) {
+    // Pick a remaining element
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
 let locations: Vector3[] = [];
 const oneVOneStart = () => {
   const players = GlobalVars.players;
+  const shuffledPlayers = shuffleArray(players);
   locations = new Array(players.length);
   world.sendMessage(
     `§a1v1 Arena start now, kill your oppenent first to get back to your original location!`
